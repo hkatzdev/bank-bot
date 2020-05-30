5b6749d2575e01dd903263ad0fabe0496f9cb8d0
Harrison Katz <git@hkatz.dev>
2020-05-30 21:37:12 +0000

    start adding ank api

--- _router.ts
+++ _router.ts
@@ -1,7 +1,6 @@
 import {
   Status,
   Router,
-  Application,
 } from "./deps.ts";
 
 import {
@@ -32,11 +31,24 @@ router
       const jsonBody = JSON.parse(context.state.body);
       context.response.body = jsonBody.challenge;
     },
-  ) //410
+  )
+  // Legacy Banker API
+  .post("/give", async (context) => {
+    const jsonBody = context.request.body();
+    const fixedBody = {
+      payer: jsonBody.bot_id || jsonBody.give_id,
+      receiver: jsonBody.send_id,
+      amount: jsonBody.gp,
+      key: jsonBody.token,
+      reason: jsonBody.reason,
+    };
+  })
   .post("/fine", async (context) => {
     context.response.status = Status.Gone;
     context.response.body =
       "Will never be implemented due to potential abuse - url reserved for legacy purposes";
-  });
+  })
+  // New Ank API
+  .use("/api");
 
 export { router };
