f54a3a1e485469562da5afbd430cf7f28834bc49
Harrison Katz <git@hkatz.dev>
2020-05-30 17:48:26 +0000

    Writing some middleware - oak/koa middleware rocks

--- _middleware.ts
+++ _middleware.ts
@@ -1,6 +1,18 @@
 import {
+  Status,
   Middleware,
   verifySlackRequest,
 } from "./deps.ts";
 
-export const bodyState: Middleware = async () => 1;
+export const bodyState: Middleware = async (context, next) => {
+  context.state.body = await context.request.body();
+  await next();
+};
+
+export const verifySlackHeader: Middleware = async (context, next) => {
+  const status = verifySlackRequest(
+    context.request.headers,
+    context.state.body,
+  );
+  await next();
+};
