d58c4f91d74379d31cd50e896b903a3da64b7dc0
Harrison Katz <git@hkatz.dev>
2020-05-30 21:37:12 +0000

    start adding ank api

---
+++ _ank.ts
@@ -0,0 +1,22 @@
+import {
+  Status,
+  Router,
+} from "./deps.ts";
+
+// New Ank API
+const ank_router = new Router();
+ank_router
+  .get("/balance", async (context) => {
+
+  })
+  .get("/users", async (context) => {
+
+  })
+  .get("/starting-amount", async (context) => {
+
+  })
+  .get("/transfer", async (context) => {
+    
+  })
+
+export { ank_router };
