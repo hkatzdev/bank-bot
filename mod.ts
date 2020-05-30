efe75136c18ed3f8a5a824b2c5d01857f801df97
Harrison Katz <git@hkatz.dev>
2020-05-30 20:58:54 +0000

    refactor and start adding legacy methods

--- mod.ts
+++ mod.ts
@@ -1,41 +1,14 @@
 import {
-  Router,
   Application,
 } from "./deps.ts";
 
 import {
-  responseTimeHeader,
-} from "./_responseTimeHeader.ts";
+  router,
+} from "./_router.ts";
 
 import {
-  rawBodyState,
-  verifySlackHeader,
-  verifyAPI,
-} from "./_middleware.ts";
-
-const router = new Router();
-router
-  .get(
-    "/",
-    async (context) => {
-      context.response.redirect(
-        "https://github.com/hkatzdev/bank-bot/tree/prohacks",
-      );
-    },
-  )
-  .get("/ping", async (context) => {
-    context.response.body = "pong!";
-  })
-  .post(
-    "/events",
-    rawBodyState,
-    verifySlackHeader,
-    verifyAPI,
-    async (context) => {
-      const jsonBody = JSON.parse(context.state.body);
-      context.response.body = jsonBody.challenge;
-    },
-  );
+  responseTimeHeader,
+} from "./_responseTimeHeader.ts";
 
 const app = new Application();
 app.use(responseTimeHeader);
