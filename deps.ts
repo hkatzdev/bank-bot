76a3a768ba4d980b3d21f1c1c85f875916d4615d
Harrison Katz <git@hkatz.dev>
2020-05-30 16:56:00 +0000

    fix deps, add response middleware

--- deps.ts
+++ deps.ts
@@ -1,12 +1,9 @@
-import {
-    verifySlackRequest,
+export {
+  verifySlackRequest,
 } from "https://denopkg.com/hkatzdev/verify_slack/mod.ts";
 
-import {
-    Application,
-    Middleware,
-} from "https://deno.land/x/oak@5.0.0/mod.ts";
-
-import {
-    responseTypeHeader,
-} from "https://deno.land/x/oak_middleware@0.1.0/observability/response_time_header.ts";
+export {
+  Router,
+  Middleware,
+  Application,
+} from "https://deno.land/x/oak@v5.0.0/mod.ts";
