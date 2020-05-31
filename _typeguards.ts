19f942fb0fa5e30f18bffca99840fe4585640cdc
Harrison Katz <git@hkatz.dev>
2020-05-31 05:19:08 +0000

    gave up on generics, manually making typeguards

--- _typeguards.ts
+++ _typeguards.ts
@@ -1,23 +1,43 @@
-export interface Balance {
+interface Balance {
   id: string;
 }
 
-export interface Users {
+export const isBalance = (object: unknown): object is Balance => {
+  return object && typeof (object as Balance).id === "string";
+};
+
+interface Users {
   lower?: number;
   upper?: number;
-  amount: number;
-  key: string;
 }
 
-export interface StartAmt {}
+export const isUsers = (object: unknown): object is Users => {
+  const tester = object as Users;
+  return object && (!tester.lower || typeof tester.lower === "number") &&
+    (!tester.upper || typeof tester.upper === "number");
+};
 
-export interface Payment {
+interface StartAmt {}
+
+export const isStartAmt = (object: unknown): object is StartAmt => {
+  return !!object;
+};
+
+interface Payment {
   payer: string;
   receiver: string;
   amount: number;
   key: string;
   reason?: string;
 }
+
+export const isPayment = (object: unknown): object is Payment => {
+  const tester = object as Payment;
+  return typeof tester.payer === "string" &&
+    typeof tester.receiver === "string" &&
+    typeof tester.amount === "number" && typeof tester.key === "string" &&
+    (!tester.reason || typeof tester.reason === "string");
+};
 
 export interface Legacy {
   bot_id: string;
@@ -27,15 +47,15 @@ export interface Legacy {
   reason?: string;
 }
 
-function isLegacy(object: unknown): unknown is Legacy {
+export const isLegacy = (object: unknown): object is Legacy => {
+  if (!object) return false;
   const tester = object as Legacy;
   if (typeof tester.bot_id !== "string") {
-
+    const old = (object as { give_id: string }).give_id;
+    if (typeof old !== "string") return false;
+    tester.bot_id = old;
   }
-  return typeof tester.send_id === "string" && typeof tester.gp === "number" && typeof tester.token === "string" && (!tester.reason || typeof tester.reason === "string") && ((tester.bot_id! && typeof tester.bot_id === "string") || typeof tester.give_id === "string");
-}
-
-const safeJson = <T>(object: unknown): object is T => {
-
-  return (pet as Fish).swim !== undefined;
+  return typeof tester.send_id === "string" && typeof tester.gp === "number" &&
+    typeof tester.token === "string" &&
+    (!tester.reason || typeof tester.reason === "string");
 };
