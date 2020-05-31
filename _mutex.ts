8daebd09f202eccc4e48c8843f3ab2680801cc0e
Harrison Katz <git@hkatz.dev>
2020-05-31 07:13:22 +0000

    mutex progress

--- _mutex.ts
+++ _mutex.ts
@@ -1,9 +1,29 @@
 class Mutex {
   #lock = Promise.resolve();
 
-  async safeCall<T>(lel: PromiseLike<T>): Promise<T> {
-    await this.#lock;
-    this.#lock = Promise.resolve(lel);
-    return T;
+  safeCall<T>(call: PromiseLike<T>): Promise<T> {
+    this.#lock = this.#lock.then(call);
+    return this.#lock;
+  }
+
+  async lock(): () => unknown {
+    let resolve: () => unknown;
+    const lock = await new Promise(res => resolve = res);
+    this.#lock = this.#lock.then(lock);
+    return resolve;
+  }
+
+  release(): void {
+
+  }
+}
+
+const map: Record<string, Mutex> = {};
+
+const safeCall = async <T>(users: string[], call: PromiseLike<T>): Promise<T> {
+  const allMutex = [];
+  for (const user of users) {
+    let mutex = map.user;
+    if (!mutex) mutex = map.user = new Mutex();
   }
 }
